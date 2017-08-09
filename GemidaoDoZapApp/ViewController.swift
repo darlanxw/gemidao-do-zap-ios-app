890fcebdd365f62a418a36e4eb14422ab147c498
Darlan Borges <darlanxw@gmail.com>
2017-08-09 10:07:24 -0300

    create projec structure

---
+++ GemidaoDoZapApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GemidaoDoZapApp
+//
+//  Created by MacDD02 on 09/08/17.
+//  Copyright © 2017 darlandev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
